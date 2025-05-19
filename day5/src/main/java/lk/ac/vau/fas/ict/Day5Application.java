caa82b0ac039fbd8414616eab57df8aa5f9007db
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-19 07:34:38 +0530

    Create course model

---
+++ day5/src/main/java/lk/ac/vau/fas/ict/Day5Application.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas.ict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day5Application.class, args);
+	}
+
+}
---
+++ day5/src/main/java/lk/ac/vau/fas/ict/model/Course.java
@@ -0,0 +1,43 @@
+package lk.ac.vau.fas.ict.model;
+
+
+public class Course {
+    private String code;
+    private String name;
+    private int credits;
+
+    public Course(String code,String name,int credits)
+    {
+        super();
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+    }
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCredits() {
+		return credits;
+	}
+
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+    
+    
+    
+}
