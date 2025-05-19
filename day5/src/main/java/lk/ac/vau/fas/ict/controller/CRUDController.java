5be4fd438787be77ab53e765f0f602ed712550ab
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-19 08:02:04 +0530

    Adding new routes

--- day5/src/main/java/lk/ac/vau/fas/ict/controller/CRUDController.java
+++ day5/src/main/java/lk/ac/vau/fas/ict/controller/CRUDController.java
@@ -27,6 +27,36 @@ public class CRUDController<K,T> {
 	}
 	
 
+	@PostMapping("/add/{id}")
+    public String addNew(@RequestBody T objects, @PathVariable("id") K id) {
+        this.object.put(id, objects);
+        return "Added new object";
+    }
 	
+	
+
+	@DeleteMapping("/delete/{id}")
+	public String DeleteOne(@PathVariable("id") K id)
+	{
+		if(object.get(id) != null)
+		{
+			object.remove(id);
+			return "The Details are Deleted";
+		}
+		return "404 couldn't find the object";
+		
+	}
+	
+
+	@PutMapping("/update/{id}")
+    public String updateOne(@PathVariable("id") K id, @RequestBody T objects) 
+	{
+		 if(this.object.get(id) != null)
+		 {
+            this.object.put(id, objects);
+            return "The details are updated";
+        }
+        return "404 couldn't find the object";
+    }
 
 }
